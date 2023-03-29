d402a15fc20eaf9cec6145ae6735702ec9eda61a
Will-Wei7 <williamswei1st@gmail.com>
2023-03-29 10:15:19 -0400

    fetch fixed

--- quiz1/app.js
+++ quiz1/app.js
@@ -3,33 +3,25 @@ const axios = require('axios');
 const path = require("path");
 const bodyParser = require('body-parser');
 const app = express();
-app.use(bodyParser.urlencoded({extended: true}));
+
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
+
+app.use(express.json());
+
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'index.html'));
 });
-app.post('/university/*', async (req, res) => {
-    const request = {
-        protocol: req.protocol,
-        host: req.hostname,
-        port: req.port,
-        path: req.path,
-        query: req.query,
-        headers: req.headers,
-        url: req.originalUrl,
-    };
-    const urlParts = request.url.split("/");
-    const nameIndex = 2;
-    const name = urlParts[nameIndex];
 
-    let university = '';
+app.post('/university', async (req, res) => {
+
+    const { name } = req.body;
+    let university='';
     try {
-        // Call API to get university information
         const response = await axios.get(`http://universities.hipolabs.com/search?name=${name}`);
         university = response.data[0];
-        console.log(response.data)
-        if (university == undefined) {
-            // If the API returns an error, return the information of the RPI
+
+        if(university==undefined){
             university = {
                 name: 'Rensselaer Polytechnic Institute',
                 country: 'United States',
@@ -37,6 +29,7 @@ app.post('/university/*', async (req, res) => {
                 web_pages: ['http://www.rpi.edu/'],
             };
         }
+
     } catch (error) {
         university = {
             name: 'Rensselaer Polytechnic Institute',
@@ -45,6 +38,8 @@ app.post('/university/*', async (req, res) => {
             web_pages: ['http://www.rpi.edu/'],
         };
     }
+    console.log(university)
     res.json(university);
 });
+
 app.listen(3000, () => console.log('Server started on port 3000'));
